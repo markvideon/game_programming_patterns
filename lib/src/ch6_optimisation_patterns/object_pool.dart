be1e71e43ad7fea939210b24c742c4183f139cdd
markvideon <mark.videon@gmail.com>
2023-11-22 20:14:07 +1030

    Object pool abstract definition

--- lib/src/ch6_optimisation_patterns/object_pool.dart
+++ lib/src/ch6_optimisation_patterns/object_pool.dart
@@ -1,2 +1,18 @@
-// todo:
 // https://gameprogrammingpatterns.com/object-pool.html
+abstract class ObjectPool<T> {
+  ObjectPool({
+    required this.builder,
+    required this.isEnabled,
+    required this.length,
+  }) {
+    elements = List.generate(length, (index) => builder(index));
+  }
+
+  final T Function(int) builder;
+  final bool Function(T) isEnabled;
+  final int length;
+  late final List<T> elements;
+
+  void enable();
+  void disable();
+}
