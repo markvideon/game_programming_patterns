e4211cb75356e6f378d4d5b9fdff4da3e92de66c
markvideon <mark.videon@gmail.com>
2023-11-24 14:18:29 +1030

    Added comments regarding Type object

--- lib/src/ch4_behavioural_patterns/type_object.dart
+++ lib/src/ch4_behavioural_patterns/type_object.dart
@@ -1,2 +1,6 @@
-// todo:
-// https://gameprogrammingpatterns.com/type-object.html
+/// The Type object pattern involves moving code outside of "strong"
+/// type constraints imposed by a language and defining associated state
+/// with data.
+///
+/// Therefore, an implementation should be application specific.
+/// https://gameprogrammingpatterns.com/type-object.html
--- lib/src/ch6_optimisation_patterns/spatial_partition.dart
+++ lib/src/ch6_optimisation_patterns/spatial_partition.dart
@@ -1,2 +1,29 @@
-// todo:
-//  https://gameprogrammingpatterns.com/spatial-partition.html
+/// Source: https://gameprogrammingpatterns.com/spatial-partition.html
+/// 
+/// The basic requirements for this pattern are that you have a set of objects
+/// that each have some kind of position and that you are doing enough
+/// queries to find objects by location that your performance is suffering.
+abstract class SpatialPartition<T> {
+  final Map<String, List<T>> _lookup = {};
+
+  void insert(String key, T candidate) {
+    if (!_lookup.containsKey(key)) {
+      _lookup[key] = <T>[];
+    }
+    _lookup[key]!.add(candidate);
+  }
+
+  void remove(String key, T candidate) {
+    final result = _lookup[key]!.remove(candidate);
+    if (result == false) {
+      throw Exception("SpatialPartition did not contain candidate!");
+    }
+  }
+
+  void move({required String oldKey,
+    required String newKey,
+    required T candidate}) {
+    remove(oldKey, candidate);
+    insert(newKey, candidate);
+  }
+}
