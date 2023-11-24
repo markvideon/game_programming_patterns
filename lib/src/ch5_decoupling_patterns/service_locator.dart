b8110b586f6939765dbcbf238ca2a303afff1952
markvideon <mark.videon@gmail.com>
2023-11-24 14:46:59 +1030

    Added comments regarding Service Locator

--- lib/src/ch5_decoupling_patterns/service_locator.dart
+++ lib/src/ch5_decoupling_patterns/service_locator.dart
@@ -1,2 +1,17 @@
-// todo:
-// https://gameprogrammingpatterns.com/service-locator.html
+/// The Service Locator pattern involves providing an abstraction over various
+/// services used in your application, which can be really useful for mocking
+/// behaviour during tests.
+///
+/// The implementation  might look something like:
+///
+/// class Locator implements Singleton {
+///   LogService get loggingService => _loggingService;
+///   late LogService _loggingService;
+///
+///   void setLogger(LogService candidate) {
+///     _loggingService = candidate;
+///   }
+/// }
+///
+/// An implementation should be application specific.
+/// https://gameprogrammingpatterns.com/service-locator.html
