9aca0eeb8293b50b575609a2bfbff885f4c4af06
markvideon <mark.videon@gmail.com>
2023-11-24 15:08:45 +1030

    Description of subclass sandbox

--- lib/src/ch4_behavioural_patterns/subclass_sandbox.dart
+++ lib/src/ch4_behavioural_patterns/subclass_sandbox.dart
@@ -1,2 +1,9 @@
-// todo:
-// https://gameprogrammingpatterns.com/subclass-sandbox.html
+/// Subclass sandbox involves making use of base classes (abstract or otherwise)
+/// to describe a common set of expected behaviours among many different kinds
+/// of entities.
+///
+/// E.g. a base Superpower class and various extending classes that define
+/// behaviour associated with Flight, Invisibility, etc.
+///
+/// Implementation of this pattern should be application-specific.
+/// https://gameprogrammingpatterns.com/subclass-sandbox.html
