834fc936515ef21cf47231402772bb8f0c1b592c
markvideon <mark.videon@gmail.com>
2023-11-28 17:31:54 +1030

    Component fundamental pieces

--- lib/src/ch5_decoupling_patterns/component.dart
+++ lib/src/ch5_decoupling_patterns/component.dart
@@ -1,3 +1,18 @@
+/// An application using a component system could have many different
+/// kinds of components.
+///
+/// Components are usually attached to some kind of base object, such as the
+/// GameObject in the Unity Engine.
 ///
 /// https://gameprogrammingpatterns.com/component.html
+abstract class GameObject {
+  List<Component> components = [];
+
+  T addComponent<T extends Component>();
+
+  bool removeComponent(Component candidate) {
+    return components.remove(candidate);
+  }
+}
+
 abstract class Component {}
