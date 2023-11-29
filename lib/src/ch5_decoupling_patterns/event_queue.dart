c67b8f140142998a6713a2c5e4f4d762c06f6300
markvideon <mark.videon@gmail.com>
2023-11-29 13:47:37 +1030

    Added comments to EventQueue

--- lib/src/ch5_decoupling_patterns/event_queue.dart
+++ lib/src/ch5_decoupling_patterns/event_queue.dart
@@ -1,2 +1,8 @@
-// todo:
-// https://gameprogrammingpatterns.com/event-queue.html
+/// An event queue... queues events. E.g. An audio playlist, input events.
+///
+/// A [Stream] may be a suitable structure, users may also be tempted by
+/// [ListQueue] which *does not* remain at its fixed capacity...
+///
+/// TBD whether implementing a custom data structure is worthwhile.
+///
+/// https://gameprogrammingpatterns.com/event-queue.html
